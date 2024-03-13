8cb17e3399f8bf5afe85194ce5988b82d726d134
reiv <javierromaynieto@gmail.com>
2024-03-13 11:19:43 +0100

    Books can now be added with new Object Constructor Book

--- app.js
+++ app.js
@@ -2,49 +2,7 @@ const myLibrary = [
     {
         title: "Game of Thrones",
         author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
-        read: true,
-    },
-    {
-        title: "Game of Thrones",
-        author: "George RR Martin",
-        pages: 824,
+        nPages: 824,
         read: true,
     },
 ];
@@ -54,9 +12,17 @@ function addBookToLibrary(book) {
     return book;
 }
 
+function Book(title, author, nPages, read) {
+    this.title = title;
+    this.author = author;
+    this.nPages = nPages;
+    this.read = read;
+}
+
 const libraryContainer = document.querySelector(".library-container");
 
 function displayLibrary() {
+    libraryContainer.innerHTML = "";
     myLibrary.forEach((book) => {
         const titleH2 = document.createElement("h2");
         const authorP = document.createElement("p");
@@ -65,8 +31,8 @@ function displayLibrary() {
 
         titleH2.textContent = book.title;
         authorP.textContent = book.author;
-        pagesP.textContent = book.pages;
-        readP.textContent = "placeholder";
+        pagesP.textContent = book.nPages;
+        readP.textContent = book.read === true ? "Read" : "Not read";
 
         const card = document.createElement("div");
         card.classList.add("card");
@@ -92,4 +58,20 @@ openDialogBtn.addEventListener("click", function() {
 
 closeDialogBtn.addEventListener("click", function() {
     modal.close();
+})
+
+const addBookBtn = document.querySelector("#add-book-btn");
+const newBookForm = document.querySelector("#new-book-form");
+
+addBookBtn.addEventListener("click", function() {
+    const newTitle = newBookForm.elements.title.value;
+    const newAuthor = newBookForm.elements.author.value;
+    const newPages = parseInt(newBookForm.elements.pages.value);
+    const newRead = newBookForm.elements.read.value === "yes" ? true : false;
+
+    const newBook = new Book(newTitle, newAuthor, newPages, newRead);
+
+    addBookToLibrary(newBook);
+    displayLibrary();
+
 })
